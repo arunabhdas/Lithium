dca4e56a554559114fb19ba029eb01e0de0b3798
Arunabh Das <arunabhdas@gmail.com>
2024-02-23 02:15:20 -0500

    Added environment.ts to .gitignore

--- lithium/src/app/services/movie.service.ts
+++ lithium/src/app/services/movie.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
+  private http = inject(HttpClient);
   constructor() { }
+
+  getTopRatedMovies(page = 1) {
+    return this.http.get()
+  }
+
+  getMovieDetails() {}
+
 }
--- lithium/src/environments/environment_bak.ts
+++ lithium/src/environments/environment_bak.ts
@@ -2,10 +2,14 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false,
-  
+  apiUrl: undefined, // add here your variables
+  apiKey: '', // add here your variables
+  baseUrl: undefined,
+  production: false
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
