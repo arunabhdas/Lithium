304a002074da7c3a68d90e43a212d785d48b06d7
Arunabh Das <arunabhdas@gmail.com>
2024-02-24 01:08:21 -0500

    Added route to detail

--- lithium/src/app/details/details.page.ts
+++ lithium/src/app/details/details.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, WritableSignal, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { MovieService } from '../services/movie.service';
+import { MovieResult } from '../services/interfaces';
 
 @Component({
   selector: 'app-details',
@@ -11,6 +13,18 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class DetailsPage implements OnInit {
+  private movieService = inject(MovieService);
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+  public movie: WritableSignal<MovieResult | null> = signal(null);
+
+  @Input()
+  set id(movieId: string) {
+    this.movieService.getMovieDetails(movieId).subscribe((movie)=> {
+      console.log(movie);
+      this.movie.set(movie);
+    });
+  }
+
 
   constructor() { }
 
