aba2e8930b4561e816bfdc314a21d926d016652c
Arunabh Das <arunabhdas@gmail.com>
2024-02-24 04:04:45 -0500

    Implemented home-defer

--- lithium/src/app/home-defer/home-defer.page.ts
+++ lithium/src/app/home-defer/home-defer.page.ts
@@ -1,20 +1,96 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { Component, inject } from '@angular/core';
+import { 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent,
+  IonList, 
+  IonItem,
+  IonSkeletonText,
+  IonAvatar,
+  IonAlert,
+  IonLabel,
+  IonBadge,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  InfiniteScrollCustomEvent, 
+} from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
+import { catchError, finalize } from 'rxjs';
+import { MovieResult } from '../services/interfaces';
+import { DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-defer',
-  templateUrl: './home-defer.page.html',
-  styleUrls: ['./home-defer.page.scss'],
+  templateUrl: 'home-defer.page.html',
+  styleUrls: ['home-defer.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonList, 
+    IonItem, 
+    IonSkeletonText, 
+    IonAvatar,
+    IonAlert,
+    IonLabel,
+    IonBadge,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+    DatePipe,
+    RouterModule
+  ],
 })
-export class HomeDeferPage implements OnInit {
+export class HomeDeferPage {
 
-  constructor() { }
-
-  ngOnInit() {
+  private movieService = inject(MovieService);
+  private currentPage = 1;
+  public error = null;
+  public isLoading = false;
+  public movies: MovieResult[] = [];
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+  public dummyArray = new Array(5);
+  constructor() {
+    this.loadMovies();
   }
 
+  loadMovies(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+
+    if (!event) {
+      this.isLoading = true;
+    }
+
+    this.movieService.getTopRatedMovies(this.currentPage).pipe(
+      finalize(() => {
+        this.isLoading = false;
+        if (event) {
+          event.target.complete();
+        }
+      }),
+      catchError((err: any) => {
+        console.log(err);
+        this.error = err.error.status_message;
+        return [];
+      })
+    ).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.movies.push(...res.results);
+        if (event) {
+          event.target.disabled = res.total_pages === this.currentPage
+        }
+      }
+
+    })
+
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent) {
+    this.currentPage++;
+    this.loadMovies(event);
+  }
 }
