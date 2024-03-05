2734a98cf6844aed3fbeed89e9d43019ffe8456b
Arunabh Das <arunabhdas@gmail.com>
2024-03-05 12:23:39 -0500

    Created chat component

--- lithium-next/lithium-web-app/src/components/home-dashboard.tsx
+++ lithium-next/lithium-web-app/src/components/home-dashboard.tsx
@@ -47,6 +47,13 @@ export function HomeDashboard() {
               </Link>
               <Link
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                href="chat"
+              >
+                <UploadIcon className="h-4 w-4" />
+                Chat
+              </Link>
+              <Link
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                 href="#"
               >
                 <UsersIcon className="h-4 w-4" />
