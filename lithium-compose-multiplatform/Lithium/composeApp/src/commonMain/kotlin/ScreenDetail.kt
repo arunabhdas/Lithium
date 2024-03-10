0e46b062a29b26da05c68aa47182a10f0e658376
Arunabh Das <arunabhdas@gmail.com>
2024-03-10 05:29:53 -0400

    Implemnented ScreenHome and ScreenDetail

---
+++ lithium-compose-multiplatform/Lithium/composeApp/src/commonMain/kotlin/ScreenDetail.kt
@@ -0,0 +1,30 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
+
+
+class ScreenDetail: Screen {
+    @Composable
+    override fun Content() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Detail")
+            Button(
+                onClick = {
+
+                }
+            ) {
+                Text("Navigate back to Home")
+            }
+        }
+    }
+}
---
+++ lithium-compose-multiplatform/Lithium/composeApp/src/commonMain/kotlin/ScreenHome.kt
@@ -0,0 +1,29 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
+
+class ScreenHome: Screen {
+    @Composable
+    override fun Content() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Home")
+            Button(
+                onClick = {
+
+                }
+            ) {
+                Text("Navigate to Detail")
+            }
+        }
+    }
+}
