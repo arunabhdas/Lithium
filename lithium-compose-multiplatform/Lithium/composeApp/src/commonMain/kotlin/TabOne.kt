6e462447e2b509ab278398ed69065964d280ea37
Arunabh Das <arunabhdas@gmail.com>
2024-03-11 01:59:09 -0400

    Implemented TabOne, TabTwo, TabThree

---
+++ lithium-compose-multiplatform/Lithium/composeApp/src/commonMain/kotlin/TabOne.kt
@@ -0,0 +1,35 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+
+object TabOne: Tab{
+    @Composable
+    override fun Content() {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Tab One")
+        }
+    }
+
+    override val options: TabOptions
+        @Composable
+        get() {
+            val index: UShort = 0u
+            val title = "Tab One"
+            val icon = rememberVectorPainter(Icons.Default.Home)
+
+            return TabOptions(
+                index, title, icon
+            )
+        }
+}
---
+++ lithium-compose-multiplatform/Lithium/composeApp/src/commonMain/kotlin/TabThree.kt
@@ -0,0 +1,37 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+
+
+object TabThree: Tab {
+    @Composable
+    override fun Content() {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Tab Three")
+        }
+    }
+
+    override val options: TabOptions
+        @Composable
+        get() {
+            val index: UShort = 0u
+            val title = "Tab Three"
+            val icon = rememberVectorPainter(Icons.Default.Notifications)
+
+            return TabOptions(
+                index, title, icon
+            )
+        }
+}
